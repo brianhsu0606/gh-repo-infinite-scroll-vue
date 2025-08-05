b651371a8fce4f90b64b3c3524ae14655e3b73be
Brian Hsu <a0938406813@gmail.com>
2025-08-05 12:23:02 +0800

    feat: 建立 fetchRepo 函式並顯示 vuejs 的前 10 筆 GitHub repos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const repoList = ref([])
+
+const fetchRepo = async () => {
+  try {
+    const res = await axios.get('https://api.github.com/users/vuejs/repos?per_page=10&page=1')
+    console.log(res.data)
+    repoList.value = res.data
+  } catch (error) {
+    console.error('Error fetching repositories:', error)
+    return []
+  }
+}
+
+onMounted(() => {
+  fetchRepo()
+})
+</script>
 
 <template>
-  <div></div>
+  <div>{{ repoList }}</div>
 </template>
 
 <style scoped lang="scss"></style>
